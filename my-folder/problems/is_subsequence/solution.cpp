12aa0de88c20c19df88279cb08d61a864a434282
sooraj1002 <91770496+sooraj1002@users.noreply.github.com>
2022-10-18 16:09:04 +0000

    Sync LeetCode submission - Is Subsequence (cpp)

---
+++ my-folder/problems/is_subsequence/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        
+        int k=0;
+        for(int i=0;i<t.size();i++)
+        {
+            if(t[i]==s[k])
+                k++;
+        }
+        return k==s.size();
+    }
+};
