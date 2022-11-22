8a8b4faccafa00a0a16619df8a08a5863bb79b2b
sooraj1002 <91770496+sooraj1002@users.noreply.github.com>
2022-11-22 06:45:18 +0000

    Sync LeetCode submission - Hamming Distance (cpp)

---
+++ my-folder/problems/hamming_distance/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int z= x^y;
+        int count=0;
+        while(z>0)
+        {
+            if(z%2==1)
+            count++;
+            z=z/2;
+        }
+        return count;
+    }
+};
