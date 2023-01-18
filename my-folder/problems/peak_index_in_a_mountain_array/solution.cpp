5f4c0af088b8609211aab29164b336aa09cbf79a
sooraj1002 <91770496+sooraj1002@users.noreply.github.com>
2023-01-18 00:01:13 +0000

    Sync LeetCode submission - Peak Index in a Mountain Array (cpp)

---
+++ my-folder/problems/peak_index_in_a_mountain_array/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int start=0;
+        int end=arr.size()-1;
+        int ans;
+        while(start <end)
+        {
+            int mid=start+(end-start)/2;
+            if(arr[mid]>arr[mid+1])
+            end=mid; //not mid-1 as we dont know if mid is max or not
+
+            else if(arr[mid]<arr[mid+1])
+            start=mid+1;
+        }
+        return start;
+    }
+};
