202e25d6852855ae2d2cb3318e5f7f88562fe99a
sooraj1002 <91770496+sooraj1002@users.noreply.github.com>
2023-01-28 22:41:46 +0000

    Sync LeetCode submission - Missing Number (cpp)

--- my-folder/problems/missing_number/solution.cpp
+++ my-folder/problems/missing_number/solution.cpp
@@ -1,7 +1,22 @@
 class Solution {
 public:
+
+    void cyclicsort(vector<int> &v){
+        for(int i=0;i<v.size();i++){
+            if(v[i]==v.size())
+            continue;
+            else
+            {
+                while(v[i]<v.size() && v[i]!=i)
+                swap(v[i],v[v[i]]);
+            }
+        }
+    }
+
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        cyclicsort(nums);
+        for(auto i:nums)
+        cout<<i<<"   ";
         int i=0;
         for(;i<nums.size();i++)
         {
