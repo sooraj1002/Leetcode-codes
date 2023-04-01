44be1d8be47b5d3d5b81c853c51d0355285a0933
sooraj1002 <91770496+sooraj1002@users.noreply.github.com>
2023-04-01 13:56:00 +0000

    Sync LeetCode submission - Integer Break (cpp)

--- my-folder/problems/integer_break/solution.cpp
+++ my-folder/problems/integer_break/solution.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    int solve(int n,vector<int> &dp){
-        if(n<=1)
-            return 0;
-        if(n==2)
-            return 1;
-        if(n==3)
-            return 2;
-        if(n==4)
-            return 4;
-        if(dp[n] != -1) return dp[n];
-        int maxi=-1e9;
-        for(int i=3;i<=n;i++)
-        {
-            int half= i*(n-i);
-            int prev=0;
-            if(n-i>=0)
-            prev= i*solve(n-i,dp);
-            maxi=max(maxi,max(half,prev));
-        }
-        return dp[n]=maxi;
-    }
-    int integerBreak(int n){
-        vector<int> dp(n+1,-1);
-        return solve(n,dp);
-    }
+    // int solve(int n,vector<int> &dp){
+    //     if(n<=1)
+    //         return 0;
+    //     if(n==2)
+    //         return 1;
+    //     if(n==3)
+    //         return 2;
+    //     if(n==4)
+    //         return 4;
+            
+    //     if(dp[n] != -1) return dp[n];
+    //     int maxi=-1e9;
+    //     for(int i=3;i<=n;i++)
+    //     {
+    //         int half= i*(n-i);
+    //         int prev=0;
+    //         if(n-i>=0)
+    //         prev= i*solve(n-i,dp);
+    //         maxi=max(maxi,max(half,prev));
+    //     }
+    //     return dp[n]=maxi;
+    // }
+    // int integerBreak(int n){
+    //     vector<int> dp(n+1,-1);
+    //     return solve(n,dp);
+    // }
     
     // int integerBreak(int n) {
     //     vector<int> dp(n+1,-1e8);
@@ -39,4 +40,26 @@ public:
     //     } 
     //     return dp[n];
     // }
+    
+    int dfs(int n,int ini,vector<int> &dp){
+        if(n==1)
+            return 1;
+        int ans;
+        if(dp[n] != -1) return dp[n];
+        
+        if(n==ini)
+            ans=0;
+        else
+            ans=n;
+        for(int i=1;i<n;i++){
+            int val=dfs(i,ini,dp)*dfs(n-i,ini,dp);
+            ans=max(ans,val);
+        }
+        return dp[n]=ans;
+    }
+
+    int integerBreak(int n){
+        vector<int>dp(n+1,-1);
+        return dfs(n,n,dp);
+    }
 };
