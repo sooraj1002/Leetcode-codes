3c6d9815ad422b454bad3f4d351d139bfb9e2114
sooraj1002 <91770496+sooraj1002@users.noreply.github.com>
2023-07-28 06:19:10 +0000

    Sync LeetCode submission - Redundant Connection (cpp)

---
+++ my-folder/problems/redundant_connection/solution.cpp
@@ -0,0 +1,56 @@
+
+class DisJointSet
+{
+public:
+    vector<int> parent, size,ans;
+    DisJointSet(int n)
+    {
+        parent.resize(n + 1);
+        size.resize(n + 1, 1);
+        for (int i = 0; i <= n; i++)
+        {
+            parent[i] = i;
+        }
+    }
+
+    int findUPar(int node)
+    {
+        if (node == parent[node])
+            return node;
+
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionBySize(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        if (ulp_u == ulp_v){ // belong to same component
+            ans={u,v};
+            return;
+        }
+        if (size[ulp_u] < size[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int V=edges.size();
+        DisJointSet ds(V);
+        for(auto edge:edges){
+            ds.unionBySize(edge[0],edge[1]);
+        }
+        return ds.ans;
+    }
+};
